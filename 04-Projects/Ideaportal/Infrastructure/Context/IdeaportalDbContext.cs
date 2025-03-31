e0ecbe115be0a7560d47c255b9ed34df24c9ddfe
giver-kdk <giverbro13@gmail.com>
2025-03-31 22:24:20 +0545

    Partial View Addition and Cloudinary Profile Fix. Removed IdeaPortal

--- 04-Projects/Ideaportal/Infrastructure/Context/IdeaportalDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Infrastructure.Context
-{
-    public class IdeaportalDbContext(DbContextOptions<IdeaportalDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
